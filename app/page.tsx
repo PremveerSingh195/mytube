06db90ce80a48864cc82015470760e0dccc0b694
Premveer Singh <prem.veer2836@gmail.com>
2026-03-09 09:16:39 +0530

    header

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-<div>
-  <h1>hello world</h1>
-</div>
+    <div className="">
+      <h1>hello world</h1>
+    </div>
   );
 }
